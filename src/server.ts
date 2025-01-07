f37b257de7ffa940f488d809306c893b931b03cb
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2025-01-07 23:47:39 +0530

    New config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
-function welcome(name: string) {
-  console.log(`welcome, ${name}`);
-}
+import { Config } from './config';
+import app from './app';
 
-welcome("aniket");
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => {
+            console.log(`Running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
+
+startServer();
