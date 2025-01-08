aa4bd118867944a33b435678f8caa9d840ddc344
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2025-01-08 23:49:28 +0530

    Just checking

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome to auth');
+    res.send('Welcome to authentication');
 });
 
 export default app;
